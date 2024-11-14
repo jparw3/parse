ecb484a067aac441e1b581cad95f4a9b88a5ec00
Jack <jparw33@gmail.com>
2024-11-14 11:51:05 +0000

    Initial Commit

---
+++ parse/parseApp.swift
@@ -0,0 +1,17 @@
+//
+//  parseApp.swift
+//  parse
+//
+//  Created by Jack Willars on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct parseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
